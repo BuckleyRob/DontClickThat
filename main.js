92eaf603a7a1bb36fcecf7cb44d495e03e545095
unknown <robertbuckley@gmail.com>
2018-06-14 07:54:35 -0700

    add wood cutters and logic buying stuff

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 var wood = 0;
+var woodCutters = 0;
 function treeClick(number){
 	wood = wood + number;
 	document.getElementById("woodDisp").innerHTML = wood;
 }
+function buyWoodCutterClick(){
+var woodCutterCost = Math.floor(10 * Math.pow(1.1, woodCutters));
+	if(wood >= woodCutterCost){
+		woodCutters = woodCutters + 1;
+		wood = wood - woodCutterCost;
+		document.getElementById("woodCutters").innerHTML = woodCutters;
+		document.getElementById("woodDisp").innerHTML = wood;
+	}
+	var nextCost = Math.floor(10 * Math.pow(1.1,woodCutters));
+	document.getElementById("woodCutterCost").innerHTML = nextCost;
+}
+window.setInterval(function(){
+treeClick(woodCutters);
+}, 1000);
