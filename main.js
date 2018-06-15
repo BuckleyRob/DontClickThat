97088debe4007190605e5ae1013c1035f635e736
David Nadeau <nadeaud.d@gmail.com>
2018-06-15 12:17:14 -0700

    3 resources calculate correctly

--- main.js
+++ main.js
@@ -1,8 +1,88 @@
+class Gatherable {
+	constructor(name, quantity, cutters) {
+		this.name = name; // wood
+		this.quantity = quantity; // 12
+		this.cutters = cutters; // 2
+	};
+	initialize(){
+		this.quantity = 0;
+		this.cutters = 0;
+	}
+	addQuantity(number){
+		this.quantity = this.quantity + number;
+	}
+	setQuantity(number){
+		this.quantity = number;
+	}
+	removeQuantity(number){
+		this.quantity = this.quantity - number;
+	}
+	addCutters(number){
+		this.cutters = this.cutters + number;
+	}
+	setCutters(number){
+		this.cutters = number;
+	}
+	removeCutters(number){
+		this.cutters = this.cutters - number;
+	}
+	logit(){
+		console.log(this.name+" has "+this.quantity+" and "+this.cutters+" cutters");
+	}
+	updateUI() {
+		document.getElementById(this.name+"Cutters").innerHTML = this.cutters;
+		document.getElementById(this.name+"Disp").innerHTML = this.quantity;
+		
+		var nextCost = Math.floor(10 * Math.pow(1.1,this.cutters));
+		document.getElementById(this.name+"CutterCost").innerHTML = nextCost;
+		this.logit();
+	}	
+}
 var wood = 0;
 var woodCutters = 0;
-function treeClick(number){
-	wood = wood + number;
-	UpdateUIWood();
+
+var gWood = new Gatherable("wood",0,0);
+var gStone = new Gatherable("stone",0,0);
+var gVine = new Gatherable("vine",0,0);
+
+function gatherableClick(name, number){
+	if(name == 'wood'){
+		gWood.addQuantity(number);
+	} else if (name == 'stone'){
+		gStone.addQuantity(number);
+	} else if (name == 'vine'){
+		gVine.addQuantity(number);
+	}
+	UpdateUI();
+}
+function UpdateUI(){
+	gWood.updateUI();
+	gStone.updateUI();
+	gVine.updateUI();
+}
+function buyGatherableCutterClick(name) {
+	var numcutters;
+	var gCutterCost;
+	if(name == 'wood'){
+		gCutterCost = Math.floor(10 * Math.pow(1.1,gWood.cutters));
+		if(gWood.quantity >= gCutterCost){
+			gWood.addCutters(1);
+			gWood.removeQuantity(gCutterCost);
+		}
+	} else if (name == 'stone'){
+		gCutterCost = Math.floor(10 * Math.pow(1.1,gStone.cutters));
+		if(gStone.quantity >= gCutterCost){
+			gStone.addCutters(1);
+			gStone.removeQuantity(gCutterCost);
+		}
+	} else if (name == 'vine'){
+		gCutterCost = Math.floor(10 * Math.pow(1.1,gVine.cutters));
+		if(gVine.quantity >= gCutterCost){
+			gVine.addCutters(1);
+			gVine.removeQuantity(gCutterCost);
+		}
+	}
+	UpdateUI();
 }
 function buyWoodCutterClick(){
 	var woodCutterCost = Math.floor(10 * Math.pow(1.1, woodCutters));
@@ -53,7 +133,9 @@ window.onload = function(){
 	load();
 }
 window.setInterval(function(){
-treeClick(woodCutters);
+gatherableClick('wood',gWood.cutters);
+gatherableClick('stone',gStone.cutters);
+gatherableClick('vine',gVine.cutters);
 }, 1000);
 window.setInterval(function(){
 	save();
