494869b1a9a3b3b00c1c32095f3c114336b882fb
David Nadeau <nadeaud.d@gmail.com>
2018-07-11 13:19:57 -0700

    Adding JSON structure for TechTree

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ window.onload = function(){
 	load();
 }
 function load(){
+	initializeTechTree();
 	var params0 = {
 		name: "TestTechnology0",
 		requirements: {"Gatherables":[],"Buildings":[],"Technologies":[]}, 
@@ -120,3 +121,28 @@ window.setInterval(function(){
 window.setInterval(function(){
 	save();
 },5000);
+
+
+function loadJSON(filename,callback) {   
+
+	var xobj = new XMLHttpRequest();
+		xobj.overrideMimeType("application/json");
+	xobj.open('GET', filename, true); // Replace 'my_data' with the path to your file
+	xobj.onreadystatechange = function () {
+		  if (xobj.readyState == 4 && xobj.status == "200") {
+			// Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+			callback(xobj.responseText);
+		  }
+	};
+	xobj.send(null);  
+}
+
+function initializeTechTree(){
+	 jsonfilename = "./TechnologyTreeData.json";
+	 loadJSON(jsonfilename,function(response) {
+  // Parse JSON string into object
+    var actual_JSON = JSON.parse(response);
+	console.log(actual_JSON);
+	console.log("TechTreeInitialized");
+ });
+}
